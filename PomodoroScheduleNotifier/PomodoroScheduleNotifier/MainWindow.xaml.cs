6c3c93669f9db5b9411b8c9a2ede44d2cc19cc60
Gabriel Mukobi <gabrielmukobi@gmail.com>
2022-03-30 14:11:00 -0700

    Implement right click show and hide when lose focus

--- PomodoroScheduleNotifier/PomodoroScheduleNotifier/MainWindow.xaml.cs
+++ PomodoroScheduleNotifier/PomodoroScheduleNotifier/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -28,21 +29,91 @@ namespace PomodoroScheduleNotifier
         public MainWindow()
         {
             InitializeComponent();
+            Hide_Window();
 
             TrayIcon.Icon = new Icon(@"Resources/Icon.ico");
             TrayIcon.Visible = true;
-            TrayIcon.Text = "Tray Application";
+            TrayIcon.Text = "Pomo Schedule";
             TrayIcon.Click += TrayIcon_Click;
         }
 
         private void TrayIcon_Click(object? sender, EventArgs e)
         {
             Debug.WriteLine("Tray Icon Clicked");
+
+            System.Windows.Forms.MouseEventArgs mouseEvent = ((System.Windows.Forms.MouseEventArgs)e);
+            switch (mouseEvent.Button)
+            {
+                case MouseButtons.Left:
+                    Toggle_Silent();
+                    break;
+                case MouseButtons.Right:
+                    Show_Window(GetMousePositionWindowsForms().X);
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void Window_Deactivated(object sender, EventArgs e)
         {
-            Debug.WriteLine("Deactivated");
+            Hide_Window();
+        }
+
+        private void Hide_Window()
+        {
+            Hide();
+        }
+
+        private void Show_Window(double cursorX)
+        {
+            AdjustWindowPosition(cursorX);
+            Show();
+            Activate();
+        }
+
+        private void AdjustWindowPosition(double cursorX)
+        {
+            Screen sc = Screen.FromHandle(new WindowInteropHelper(this).Handle);
+            if (sc.WorkingArea.Top > 0)
+            {
+                Rect desktopWorkingArea = SystemParameters.WorkArea;
+                var middleOfWindow = desktopWorkingArea.Right - (Width / 2);
+                var gapToMiddle = middleOfWindow - cursorX;
+                if (gapToMiddle < 0) gapToMiddle = 0;
+                Left = desktopWorkingArea.Right - Width - gapToMiddle;
+                Top = desktopWorkingArea.Top;
+            }
+
+            else if ((sc.Bounds.Height - sc.WorkingArea.Height) > 0)
+            {
+                Rect desktopWorkingArea = SystemParameters.WorkArea;
+                var middleOfWindow = desktopWorkingArea.Right - (Width / 2);
+                var gapToMiddle = middleOfWindow - cursorX;
+                if (gapToMiddle < 0) gapToMiddle = 0;
+                Left = desktopWorkingArea.Right - Width - gapToMiddle;
+                Top = desktopWorkingArea.Bottom - Height;
+            }
+            else
+            {
+                Rect desktopWorkingArea = SystemParameters.WorkArea;
+                Left = desktopWorkingArea.Right - Width;
+                Top = desktopWorkingArea.Bottom - Height;
+            }
+        }
+
+        public static System.Windows.Point GetMousePositionWindowsForms()
+        {
+            var point = System.Windows.Forms.Control.MousePosition;
+            Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+            var pixelX = (int)((96 / g.DpiX) * point.X);
+            var pixelY = (int)((96 / g.DpiY) * point.X);
+            return new System.Windows.Point(pixelX, pixelY);
+        }
+
+        private void Toggle_Silent()
+        {
+            throw new NotImplementedException();
         }
     }
 }
