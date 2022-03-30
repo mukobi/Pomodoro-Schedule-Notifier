b937926e9726794a9d9ee6276d9da465bacf22a4
Gabriel Mukobi <gabrielmukobi@gmail.com>
2022-03-30 13:51:33 -0700

    Get a basic tray icon showing

---
+++ PomodoroScheduleNotifier/PomodoroScheduleNotifier/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PomodoroScheduleNotifier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NotifyIcon TrayIcon = new NotifyIcon();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TrayIcon.Icon = new Icon(@"Resources/Icon.ico");
+            TrayIcon.Visible = true;
+            TrayIcon.Text = "Tray Application";
+            TrayIcon.Click += TrayIcon_Click;
+        }
+
+        private void TrayIcon_Click(object? sender, EventArgs e)
+        {
+            Debug.WriteLine("Tray Icon Clicked");
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Deactivated");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pomodoro-Schedule-Notifier
 Pomodoro timer synced to a fixed schedule that lives in the Windows notification area and notifies state with an icon and optionally sound.
+
+
+## Acknowledgments
+- https://github.com/ejabu/TrayApp for some code relating to notification tray icons in WPF.
