997b7d827eb3404536f5a0a9c7659a9573ea4d36
Gabriel Mukobi <gabrielmukobi@gmail.com>
2022-04-19 19:43:03 -0700

    Generate pause icon

--- IconGenerator/generate_icons.ps1
+++ IconGenerator/generate_icons.ps1
@@ -3,6 +3,7 @@ $color_labels = @("red", "green", "blue")
 $color_hexes = @("#fe0052", "#00a807", "#0078fa")
 $max_nums = @(25, 5, 35)
 
+# Generate all the colors
 for ($color_i = 0; $color_i -lt $color_labels.length; $color_i++) {
     $color_label = $color_labels[$color_i]
     $color_hex = $color_hexes[$color_i]
@@ -12,3 +13,6 @@ for ($color_i = 0; $color_i -lt $color_labels.length; $color_i++) {
         magick -size 128x128 xc:transparent -fill $color_hex -stroke white -strokewidth 2 -draw "roundrectangle 0,0 127,127 16,16" -gravity center -font Bahnschrift -fill white -pointsize 112 -strokewidth 0 -annotate +0+5 "$number" $PSScriptRoot/../PomodoroScheduleNotifier/PomodoroScheduleNotifier/Resources/$color_label-$number.ico
     }
 }
+
+# Generate a yellow pause symbol
+magick -size 128x128 xc:transparent -fill "#f2bc18" -stroke white -strokewidth 2 -draw "roundrectangle 0,0 127,127 16,16" -gravity center -font Bahnschrift -fill white -pointsize 112 -strokewidth 0 -annotate +0+5 "P" $PSScriptRoot/../PomodoroScheduleNotifier/PomodoroScheduleNotifier/Resources/pause.ico
