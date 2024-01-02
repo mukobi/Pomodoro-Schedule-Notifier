f92d97a82c0cdf86e666e1a83f81210dca7c6fa5
Gabriel Mukobi <gabrielmukobi@gmail.com>
2024-01-02 12:44:00 -0800

    Add Choco package

---
+++ Chocolatey_Package/Pomodoro-Schedule-Notifier/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'Pomodoro-Schedule-Notifier'
+$exeName = 'PomodoroScheduleNotifier'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/mukobi/Pomodoro-Schedule-Notifier/releases/download/v1.0.0/PomodoroScheduleNotifier.v1.0.0.zip'
+$installLocation = 'C:\tools\Pomodoro-Schedule-Notifier'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $installLocation
+  url            = $url
+  checksum       = 'db1fdb5eb066299ab081aa7e84f3a30e1864b0339350246660488f1c6d48795d'
+  checksumType   = 'sha256'
+}
+
+Get-Process | Where-Object { $_.ProcessName -eq $exeName } | Stop-Process -Force -ErrorAction SilentlyContinue
+
+Install-ChocolateyZipPackage @packageArgs
+
+$WshShell = New-Object -comObject WScript.Shell
+$StartupDir = $WshShell.SpecialFolders("Startup")
+Install-ChocolateyShortcut -ShortcutFilePath "$StartupDir\PomodoroScheduleNotifier.lnk" -TargetPath "$installLocation\PomodoroScheduleNotifier.exe" -WorkingDirectory $installLocation
+
+Start-Process -FilePath "$installLocation\PomodoroScheduleNotifier.exe" -WorkingDirectory $installLocation
---
+++ Chocolatey_Package/Pomodoro-Schedule-Notifier/tools/chocolateyuninstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'Pomodoro-Schedule-Notifier'
+$exeName = 'PomodoroScheduleNotifier'
+$WshShell = New-Object -comObject WScript.Shell
+$StartupDir = $WshShell.SpecialFolders("Startup")
+$ShortcutPath = "$StartupDir\PomodoroScheduleNotifier.lnk"
+
+Get-Process | Where-Object { $_.ProcessName -eq $exeName } | Stop-Process -Force -ErrorAction SilentlyContinue
+
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $packageName
+
+$installLocation = 'C:\tools\Pomodoro-Schedule-Notifier'
+if (Test-Path -Path $installLocation) {
+  Write-Host "Removing installation directory at '$installLocation'"
+  Remove-Item -Recurse -Force $installLocation
+} else {
+  Write-Warning "Installation directory not found: '$installLocation'"
+}
+
+if (Test-Path -Path $ShortcutPath) {
+  Write-Host "Removing shortcut at '$ShortcutPath'"
+  Remove-Item -Force $ShortcutPath
+} else {
+  Write-Warning "Shortcut not found: '$ShortcutPath'"
+}
